7da60e094d647c350db1dac3fba20e9dc4d3d7f5
wesleysv <49928589+wesleysv@users.noreply.github.com>
2020-08-27 10:35:58 -0300

    Create pratica03.py

---
+++ LTP2/pratica03.py
@@ -0,0 +1,5 @@
+# usuario declara dois valores e calculamos a média
+numero1 = int(input('nuemro1:'))
+numero2 = int(input('nuemro2:'))
+media = (numero1 + numero2) / 2 
+print('o resultado:',media)
