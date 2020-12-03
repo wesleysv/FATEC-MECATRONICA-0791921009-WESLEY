b0ed7ec1c5c0a54bda02d2182054120845ced3d2
wesleysv <49928589+wesleysv@users.noreply.github.com>
2020-12-03 09:06:18 -0300

    Update prog.py

--- LTP2/trabalho/prog.py
+++ LTP2/trabalho/prog.py
@@ -1,19 +1,28 @@
 opcao = int(input("deseja converter estrela para triagulo?(digite 1) ou triangulo para estrela?(digite 2)"))
 
-if opcao == 1:
-   (print ("estrela para triagulo"))
-   rab = int (input("digite o resistor correspondente a Rab:"))
-   rac = int (input("digite o resistor correspondente a Rac:"))
-   rbc = int (input("digite o resistor correspondente a Rbc:"))
-ra = int ((rab*rac)/(rab+rac+rbc))
-rb = int ((rab*rbc)/(rab+rac+rbc))
-rc = int ((rac*rbc)/(rab+rac+rbc))
 
-if opcao == 2:
-  (print ("triagulo para estrela"))
-  ra = int (input("digite o resistor correspondente a Ra:"))
-  rb = int (input("digite o resistor correspondente a Rb:"))
-  rc = int (input("digite o resistor correspondente a Rc:"))
-rab = int (((ra*rc) + (rc*rb) + (rb*ra))/(rc))
-rac = int (((ra*rc) + (rc*rb) + (rb*ra))/(rb))
-rbc = int (((ra*rc) + (rc*rb) + (rb*ra))/(ra))
+
+if opcao == 1:(
+   (print ("estrela para triagulo"))
+rab = float (input("digite o resistor correspondente a Rab:"))
+rac = float (input("digite o resistor correspondente a Rac:"))
+rbc = float (input("digite o resistor correspondente a Rbc:"))
+ra = float ((rab*rac)/(rab+rac+rbc))
+rb = float ((rab*rbc)/(rab+rac+rbc))
+rc = float ((rac*rbc)/(rab+rac+rbc))
+
+print("RA=\t RB=\n RC=\n",ra,rb,rc))
+
+elif opcao == 2:
+  print ("triagulo para estrela")
+  ra = float (input("digite o resistor correspondente a Ra:"))
+  rb = float (input("digite o resistor correspondente a Rb:"))
+  rc = float (input("digite o resistor correspondente a Rc:"))
+ rab = float (((ra*rc) + (rc*rb) + (rb*ra))/(rc))
+ rac = float (((ra*rc) + (rc*rb) + (rb*ra))/(rb))
+ rbc = float (((ra*rc) + (rc*rb) + (rb*ra))/(ra))
+
+print("Rab=\t RAC=\n RBC=\n",ra,rb,rc)
+
+else:
+  print("não exiete essa opcao.")
